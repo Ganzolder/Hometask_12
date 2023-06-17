817ac80ea4834e8c2e286b56f0e3056f568f2acb
Ganzolder <131520341+Ganzolder@users.noreply.github.com>
2023-06-17 23:10:54 +0700

    Revert "Feature/task3"

--- str_func.py
+++
@@ -1,9 +0,0 @@
-def upper_caser(word):
-    """word for uppercase"""
-    upper_word = str(word).upper()
-    return upper_word
-
-def title_caser(word):
-    """word for title case"""
-    title_word = str(word).title()
-    return title_word
