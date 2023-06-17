4550bfed1e563280f288bd8a900d922de8df6a9d
ganzolder <gnmsapr@gmail.com>
2023-06-17 20:16:22 +0700

    added title case func and docstring

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def upper_caser(word):
+    """word for uppercase"""
     upper_word = str(word).upper()
     return upper_word
+
+def title_caser(word):
+    """word for title case"""
+    title_word = str(word).title()
+    return title_word
